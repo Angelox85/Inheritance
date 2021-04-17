3edd59b7c98662a22d649750223c9f3b3689ec94
Angel Hernandez Sierra <angel.hernandezsierr@sinclair.edu>
2021-04-17 15:44:19 -0400

    define the car class

---
+++ Car.h
@@ -0,0 +1,14 @@
+// Name : Angel E Hernandez
+// Date : April 17
+// CIS 1202.800
+// Project name : Inheritance
+
+#pragma once
+#ifndef CAR_H
+#define CAR_H
+#include "Vehicle.h"
+#include <string>
+using namespace std;
+
+// define Car class 
+
