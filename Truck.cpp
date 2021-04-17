b1a1433cb3514f0716c3af10a053cf114d75b3f5
Angel Hernandez Sierra <angel.hernandezsierr@sinclair.edu>
2021-04-17 15:40:02 -0400

    defined  display function for the truck class

---
+++ Truck.cpp
@@ -0,0 +1,11 @@
+// Name : Angel E Hernandez
+// Date : April 17
+// CIS 1202.800
+// Project name : Inheritance
+
+#pragma once
+#include <iostream>
+#include "Truck.h"
+using namespace std;
+
+// define display function for truck class
