307ef53adcb0bdfba0b3724741cca14aff668771
Angel Hernandez Sierra <angel.hernandezsierr@sinclair.edu>
2021-04-17 15:41:46 -0400

    truck :: displayInfo() will display the information for the vehicles type truck

--- Truck.cpp
+++ Truck.cpp
@@ -9,3 +9,33 @@
 using namespace std;
 
 // define display function for truck class
+
+void Truck::displayInfo()
+{
+	string manufact;
+	int vYear;
+	int cap;
+
+
+	// prompt user to enter information
+	cout << "Car: \n";
+	cout << "Enter the manufacturer: ";
+	cin.ignore();
+	getline(cin, manufact);
+	cout << "Enter the year built: ";
+	cin >> vYear;
+	cout << "Enter the towing capacity: ";
+	cin >> cap;
+
+
+	// define a vehicle object
+	Truck truck(manufact, vYear, cap);
+
+	// display information
+	cout << "Vehicle information: \n";
+	cout << "Manufacturer: " << truck.getManufacturer() << endl;
+	cout << "Year Buil: " << truck.getYear() << endl;
+	cout << "Towing capacity: " << truck.getCapacity() << endl;
+	cout << endl;
+
+}
