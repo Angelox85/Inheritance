f51d8ba198d08bed109a9e0624e8e9053ca228c5
Angel Hernandez Sierra <angel.hernandezsierr@sinclair.edu>
2021-04-17 15:34:11 -0400

    create the class truck, that inherits members and functions from the class vehicle

---
+++ Truck.h
@@ -0,0 +1,14 @@
+// Name : Angel E Hernandez
+// Date : April 17
+// CIS 1202.800
+// Project name : Inheritance
+
+#pragma once
+#ifndef TRUCK_H
+#define TRUCK_H
+#include "Vehicle.h"
+#include <string>
+using namespace std;
+
+// Truck is a type of vehicle that inherits members variables
+//and functions from the class vehicle
